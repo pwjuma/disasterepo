048fd4c795a7b0c0239e700b642c723b5fd8f4bd
MyAndela <peter.juma@andela.com>
2020-07-01 12:48:33 +0300

    Add new ENV file

--- script.rb
+++ script.rb
@@ -1,4 +1,4 @@
-require './environment'
+require_relative 'environment.rb'
 
 puts "Your username is currently set to '#{ENV['user_name']}'."
 puts "Your password is currently set to '#{ENV['password']}'."
