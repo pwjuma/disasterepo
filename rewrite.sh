aa71229abf2bebd6058b55c6e8f6f80339ab9561
peterjuma <wanyonyi.p@gmail.com>
2020-08-26 19:50:19 +0300

    Update rewrite.sh

--- rewrite.sh
+++ rewrite.sh
@@ -2,18 +2,3 @@
 #bye #bye #irony
 git filter-branch --env-filter '
 
-OLD_EMAIL="noname@example.com"
-CORRECT_NAME="Peter Juma - Andela"
-CORRECT_EMAIL="peter.juma@andela.com"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
